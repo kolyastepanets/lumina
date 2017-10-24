b074491f9ac3676c14504035828d713348c5c157
Nikolay Stepanets <nas@anadeainc.com>
2017-10-24 16:51:20 +0300

    feature/fixed header and jump to top

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+      $('.topbutton').fadeIn();
+    } else {
+      $('.topbutton').fadeOut();
+    }
+
+    if ($(this).scrollTop() >= 300) {
+      $('.main-navigation').addClass('fixed-header');
+    }
+    else {
+      $('.main-navigation').removeClass('fixed-header');
+    }
+  });
+
+  $('.topbutton').click(function(){
+    $('html, body').animate({ scrollTop : 0 }, 800);
+    return false;
+  });
+});
