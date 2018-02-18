6f0d88f38c37d33df7d5bf0e146df2eee77f995e
Nikolay Stepanets <kolyastepanets@gmail.com>
2018-02-19 00:05:34 +0200

    fix on portfolio page, default size of image

--- app/uploaders/albulm_title_uploader.rb
+++ app/uploaders/albulm_title_uploader.rb
@@ -1,6 +1,8 @@
 class AlbulmTitleUploader < BaseUploader
   include ActiveAdminJcrop::AssetEngine::CarrierWave
 
+  process resize_to_fit: [800, 800]
+
   version :thumb do
     process :active_admin_crop
     process resize_to_fill: [230, 230]
--- app/uploaders/small_article_main_image_uploader.rb
+++ app/uploaders/small_article_main_image_uploader.rb
@@ -1,6 +1,8 @@
 class SmallArticleMainImageUploader < BaseUploader
   include ActiveAdminJcrop::AssetEngine::CarrierWave
 
+  process resize_to_fit: [800, 800]
+
   version :thumb do
     process :active_admin_crop
     process resize_to_fill: [320, 260]
