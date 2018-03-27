d4ed28ef54c5b6d46951f01e7cd0e72b66c96e43
Nikolay Stepanets <nas@anadeainc.com>
2018-03-27 17:00:22 +0300

    slider on main page

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -18,4 +18,14 @@ $(document).on('turbolinks:load', function() {
     $('html, body').animate({ scrollTop : 0 }, 800);
     return false;
   });
+
+  $('.carousel-photos').slick({
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
 });
